09dd438cc558df5c9aa2f06447eb7d821a92267a
TMPO <nunezp20@gmail.com>
2023-02-14 08:49:20 -0600

    first commit

---
+++ lib/home_page.dart
@@ -0,0 +1,32 @@
+import 'dart:convert';
+
+import 'package:cripto_lists/data/constants.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var exchanges = jsonDecode(CRYPTO_EXCHANGES) as List;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Crypto APP"),
+      ),
+      body: ListView.builder(
+        itemCount: exchanges.length,
+        itemBuilder: (BuildContext context, int index) {
+          return ListTile(
+            title: Text('${exchanges[index]["name"]}'),
+          );
+        },
+      ),
+    );
+  }
+}
