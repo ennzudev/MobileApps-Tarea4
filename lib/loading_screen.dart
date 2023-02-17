1fb644a857668d865b7245b7776d4ac6353e69d7
TMPO <nunezp20@gmail.com>
2023-02-17 13:09:14 -0600

    loading screen edit

--- lib/loading_screen.dart
+++ lib/loading_screen.dart
@@ -3,12 +3,34 @@ import 'package:flutter/material.dart';
 class LoadingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    Future.delayed(Duration(seconds: 2), () {
+    Future.delayed(Duration(seconds: 5), () {
       Navigator.pushNamed(context, '/home');
     });
+
     return Scaffold(
+      backgroundColor: Colors.white,
       body: Center(
-        child: CircularProgressIndicator(),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Aquí agregas tu imagen
+            //Image.asset('assets/images/crypto.png'),
+            SizedBox(height: 20),
+            // Agrega tu nombre y número de tarea
+            Text(
+              'Pablo Nunez - Tarea 4',
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 10),
+            // Agrega el CircularProgressIndicator
+            CircularProgressIndicator(
+              valueColor: AlwaysStoppedAnimation<Color>(Colors.amber),
+            ),
+          ],
+        ),
       ),
     );
   }
